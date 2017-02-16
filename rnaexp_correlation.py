a36e96738c366ac3a0a1907c050df67e47403a1a
CodingBash <bbecerr@outlook.com>
2017-02-16 07:24:13 -0600

    Fixed correlation problem

--- rnaexp_correlation.py
+++ rnaexp_correlation.py
@@ -1,7 +1,7 @@
 from genelist_creator import read_file
 from scipy.stats.stats import pearsonr
 from queue import PriorityQueue
-from math import nan
+from math import isnan
 
 class GeneExpressionSetCorrelation:
     def __init__(self, target_gene, containing_gene, r_row):
@@ -9,7 +9,7 @@ class GeneExpressionSetCorrelation:
         self.containing_gene = containing_gene
         self.r_row = r_row
     def __lt__(self, other):
-        return self.r_row > other.r_row
+        return self.r_row < other.r_row
     def to_string(self):
         return self.target_gene.geneName + " " + self.containing_gene.geneName + " " + str(self.r_row)
         
@@ -25,14 +25,11 @@ def find_correlations(input_gene, intermediate_gene_list):
             continue
         # Determine how to utilize p_val (pearsonr[1])
         r_row = pearsonr(input_gene.rnaSeq, intermediate_gene.rnaSeq)[0]
-        # TODO: NAN not replacing
-        r_row = r_row if r_row != nan else -1
+        r_row = -1 if isnan(r_row) else r_row
         pqueue.put((r_row, GeneExpressionSetCorrelation(input_gene, intermediate_gene, r_row)))
     while not pqueue.empty():
         item = pqueue.get()
-        # TODO: not printout out in order
         print(item[1].to_string())
-        
         
 if __name__ == "__main__":
     main()
