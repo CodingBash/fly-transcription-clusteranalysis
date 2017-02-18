da4f7ed7b516b9fc15573182e5c7055ea0575101
CodingBash <bbecerr@outlook.com>
2017-02-18 09:19:40 -0600

    Made method more dynamic

--- src/data_to_xml.py
+++ src/data_to_xml.py
@@ -1,16 +1,16 @@
 from genelist_creator import read_file
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 
-def create_xml(filename):
-    payload = read_file(filename)
+def create_xml(input_filename, output_filename, is_prettify=True, gene_count=-1 ):
+    payload = read_file(input_filename)
     data = payload[0]
     root = Element("genome")
     root.set('version', '1.0.0')
     root.append(Comment("Generated XML"))
     outer_count = 0;
     for entry in data:
-        #if outer_count > 1000:
-         #   break
+        if gene_count != -1 and outer_count > gene_count:
+            break
         if len(entry.rnaSeq) == 104:
             gene_child = SubElement(root, "gene")
             gene_name_child = SubElement(gene_child, "gene-name")
@@ -22,16 +22,17 @@ def create_xml(filename):
                 rna_exp_stage_child.text = str(rna_exp_stage)
                 count+=1
             outer_count += 1
-    file = open("../res/data.xml", 'w')
-    file.write(prettify(root))
+    file = open(output_filename, 'w')
+    if is_prettify:
+        file.write(prettify(root))
+    else:
+        file.write(str(tostring(root)))
     file.close()
                 
 from xml.etree import ElementTree
 from xml.dom import minidom
 
 def prettify(elem):
-    """Return a pretty-printed XML string for the Element.
-    """
     rough_string = ElementTree.tostring(elem, 'utf-8')
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
