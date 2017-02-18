a53fcf6514e865830b071c01b53e24d105a15249
CodingBash <bbecerr@outlook.com>
2017-02-18 09:44:23 -0600

    Added xml unmarshaller for single rna tag

--- src/data_to_xml.py
+++ src/data_to_xml.py
@@ -28,6 +28,33 @@ def create_xml(input_filename, output_filename, is_prettify=True, gene_count=-1 
     else:
         file.write(str(tostring(root)))
     file.close()
+
+def create_xml_simple(input_filename, output_filename, is_prettify=True, gene_count=-1 ):
+    payload = read_file(input_filename)
+    data = payload[0]
+    root = Element("genome")
+    root.set('version', '1.0.0')
+    root.append(Comment("Generated XML"))
+    outer_count = 0;
+    for entry in data:
+        if gene_count != -1 and outer_count > gene_count:
+            break
+        if len(entry.rnaSeq) == 104:
+            gene_child = SubElement(root, "gene")
+            gene_name_child = SubElement(gene_child, "gene-name")
+            gene_name_child.text = entry.geneName
+            rna_exp_child = SubElement(gene_child, "rna-exp")
+            rna_exp_text = ""
+            for rna_exp_stage in entry.rnaSeq:
+                rna_exp_text += str(rna_exp_stage) + ","
+            rna_exp_child.text = rna_exp_text[:-1]
+            outer_count += 1
+    file = open(output_filename, 'w')
+    if is_prettify:
+        file.write(prettify(root))
+    else:
+        file.write(str(tostring(root)))
+    file.close()
                 
 from xml.etree import ElementTree
 from xml.dom import minidom
