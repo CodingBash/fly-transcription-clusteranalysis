0fed3d2933de7f8c005ae9107a4f227c2cbd262c
CodingBash <bbecerr@outlook.com>
2017-05-10 12:48:41 -0500

    Updated methods to marshall xml for gene rna data (semi-tested)

--- src/data_to_xml.py
+++ src/data_to_xml.py
@@ -1,9 +1,13 @@
-from genelist_creator import read_file
+from genelist_creator import read_file_rnadata
+from genelist_creator import read_file_geneid
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
 
+# DEPRECATED: The secondary column and gene name was removed from the corresponding xml. Those columns are now read in the gene_id xml marshaller
+#   
+#    
 # Creates XML from data where each rna stage is a new child element
-def create_xml(input_filename, output_filename, is_prettify=True, gene_count=-1 ):
-    payload = read_file(input_filename)
+def rna_exp_marshall_xml(input_filename, output_filename, is_prettify=True, gene_count=-1 ):
+    payload = read_file_rnadata(input_filename)
     data = payload[0]
     root = Element("genome")
     root.set('version', '1.0.0')
@@ -38,8 +42,42 @@ def create_xml(input_filename, output_filename, is_prettify=True, gene_count=-1 
 # output_filename = the output XML file
 # is_prettify = Boolean representing whether the XML file should be prettified
 # gene_count = limit of how many genes should be included in the XML. -1 is all genes in TSV
-def create_xml_simple(input_filename, output_filename, is_prettify=True, gene_count=-1 ):
-    payload = read_file(input_filename)
+def rna_exp_marshall_xml_simple(input_filename, output_filename, is_prettify=True, gene_count=-1 ):
+    payload = read_file_rnadata(input_filename)
+    data = payload[0]
+    root = Element("genome-rna-data")
+    root.set('version', '1.0.0')
+    root.append(Comment("Generated XML by @CodingBash"))
+    outer_count = 0;
+    for entry in data:
+        if gene_count != -1 and outer_count > gene_count:
+            break
+        if len(entry.rnaSeq) == 104:
+            gene_child = SubElement(root, "gene")
+            gene_db_id_child = SubElement(gene_child, "gene-db-id")
+            gene_db_id_child.text = entry.dbIdentifier
+            rna_exp_child = SubElement(gene_child, "rna-exp")
+            rna_exp_text = ""
+            for rna_exp_stage in entry.rnaSeq:
+                rna_exp_text += str(rna_exp_stage) + ","
+            rna_exp_child.text = rna_exp_text[:-1]
+            outer_count += 1
+    file = open(output_filename, 'w')
+    if is_prettify:
+        file.write(prettify(root))
+    else:
+        file.write(str(tostring(root)))
+    file.close()
+                
+# Creates XML from data. This will marshall the the gene information (gene ID, secondary ID, name, and synonyms) to an XML
+#
+# PARAMS
+# input_filename = Input TSV file that will be given to read_file() (read documentation over there)
+# output_filename = the output XML file
+# is_prettify = Boolean representing whether the XML file should be prettified
+# gene_count = limit of how many genes should be included in the XML. -1 is all genes in TSV
+def gene_id_marshall_xml_simple(input_filename, output_filename, is_prettify=True, gene_count=-1 ):
+    payload = read_file_rnadata(input_filename)
     data = payload[0]
     root = Element("genome")
     root.set('version', '1.0.0')
@@ -68,7 +106,10 @@ def create_xml_simple(input_filename, output_filename, is_prettify=True, gene_co
     else:
         file.write(str(tostring(root)))
     file.close()
-                
+    
+    
+    
+    
 from xml.etree import ElementTree
 from xml.dom import minidom
 
@@ -85,7 +126,7 @@ def prettify(elem):
 
 if __name__ == '__main__':
     # The input file contains all RNA expression data for all genes available in D. m
-    create_xml_simple("../res/results_w_more_ids.tsv", "../res/genedata_w_ids.xml")
+    rna_exp_marshall_xml_simple("../res/tsv_files/rna_data_limited_ids.tsv", "../res/xml_files/genome-rna-data-1.xml")
     #read_file("../res/results_w_more_ids.tsv")
         
              
--- src/genelist_creator.py
+++ src/genelist_creator.py
@@ -1,12 +1,22 @@
 import csv
+from pprint import pprint
 
-class GeneExpressionSet:
-    def __init__(self, dbIdentifier='', secondaryIdentifier='', geneName='', rnaSeq = []):
+class GeneRnaDataSet:
+    def __init__(self, dbIdentifier='', rnaSeq = []):
+        self.dbIdentifier = dbIdentifier
+        self.rnaSeq = rnaSeq
+    def __str__(self):
+        return self.dbIdentifier + " " +  str(self.rnaSeq)
+
+class GeneIdentifierSet:
+    def __init__(self, dbIdentifier='', secondaryIdentifier='', geneName='', synonyms = []):
         self.dbIdentifier = dbIdentifier
         self.secondaryIdentifier = secondaryIdentifier
         self.geneName = geneName
-        self.rnaSeq = rnaSeq
-
+        self.synonyms = synonyms
+        
+    
+# Works    
 # Unmarshalls a TSV file containing gene's expression stage values
 #
 # Takes in a TSV file with 5 columns
@@ -16,13 +26,18 @@ class GeneExpressionSet:
 # COL 3: RNA Expression Stage
 # COL 4: RNA Expression Score
 #
+# Takes in a TSV file with 3 columns
+# COL 0 : Gene DB Identifier
+# COL 1: RNA Expression Stage
+# COL 2: RNA Expression Score
+#
 # Returns a List of GeneExpressionSet objects 
 #
 # This method usually acts as a subroutine for another method, not a standolone function
-def read_file(filename):
+def read_file_rnadata(filename):
     with open(filename,'rt') as tsvin:
         tsvin = csv.reader(tsvin, delimiter='\t')
-        gene = GeneExpressionSet()
+        gene = GeneRnaDataSet()
         geneList = []
         rnaSeq = []
         count = 0
@@ -31,20 +46,68 @@ def read_file(filename):
             if geneString != row[0]:
                 gene.rnaSeq = rnaSeq
                 geneList.append(gene)
-                gene = GeneExpressionSet()
+                gene = GeneRnaDataSet()
                 rnaSeq = []
+                gene.dbIdentifier = geneString = row[0]
+                
+            rnaSeq.append(int(row[2]))
+            count += 1
+        geneList = geneList[1:]
+        return [geneList, 0, max]
+
+
+# Unmarshalls a TSV file containing gene's expression stage values
+#
+# Takes in a TSV file with 4 columns
+# COL 0 : Gene DB Identifier
+# COL 1: Gene Secondar Identifier
+# COL 2: Gene Name
+# COL 3: Gene Synonym
+#
+# Returns a List of GeneExpressionSet objects 
+#
+# This method usually acts as a subroutine for another method, not a standolone function
+def read_file_geneid(filename):
+    with open(filename,'rt') as tsvin:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+        gene = GeneIdentifierSet()
+        geneList = []
+        synonyms = []
+        count = 0
+        geneString = ""
+        for row in tsvin:
+            if geneString != row[0]:
+                gene.synonyms = synonyms
+                geneList.append(gene)
+                gene = GeneIdentifierSet()
+                synonyms = []
                 gene.dbIdentifier = geneString = row[0]
                 gene.secondaryIdentifier = row[1]
                 gene.geneName = row[2]
                 
-            rnaSeq.append(int(row[4]))
+            synonyms.append(int(row[4]))
             count += 1
         geneList = geneList[1:]
         return [geneList, 0, max]
 
 if __name__ == '__main__':
     # This file contains all RNA expression data for all genes available in D. m
-    read_file("../res/results_w_more_ids.tsv")
+    #read_file("../res/results_w_more_ids.tsv") To access deprecated read_file method, go to commit #db4a6ee
+    
+    for geneRnaDataSet in read_file_rnadata("../res/tsv_files/rna_data_limited_ids.tsv"):
+        print(geneRnaDataSet.dbIdentifier)
+    #pprint(read_file_geneid("../res/tsv_files/gene_identifiers.tsv"))          
     
     # This will not work on new code since it lacks all required columns. Refer to old version of code
     # read_file("../res/results_w_more_ids.tsv")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
